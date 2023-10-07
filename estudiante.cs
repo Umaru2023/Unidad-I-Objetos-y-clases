411f658f077190f49d99f28d101c3ffaac82c9a9
Nestor Caceres <nestor.caceres@curs.unanleon.edu.ni>
2023-10-07 00:26:58 -0600

    Update estudiante.cs

--- estudiante.cs
+++ estudiante.cs
@@ -2,7 +2,16 @@ using System;
 
 public class Estudiante
 {
+   
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+    public string Curso { get; set; }
+
     
+    public Estudiante(string nombre, int edad, string curso)
+    {
+        
+    }
 }
 
 class Program
