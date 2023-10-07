c0684fd0f62ad06d63f9972ac35ab80a48b5adfa
Nestor Caceres <nestor.caceres@curs.unanleon.edu.ni>
2023-10-07 00:26:08 -0600

    Create libro.cs
    3. Creación de Clase Libro:
    Define una clase "Libro" con propiedades como "Título" y "Autor."
    Agrega un método para mostrar la información del libro.
    Crea objetos "Libro" y llama al método para mostrar su información.

---
+++ libro.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class Libro
+{
+    // Propiedades
+    public string Título { get; set; }
+    public string Autor { get; set; }
+
+    // Método para mostrar información del libro
+    public void MostrarInformación()
+    {
+        
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("¡Hola, mundo!");
+    }
+}
