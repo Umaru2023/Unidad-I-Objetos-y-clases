c1a6c7751a54740fc2930347e33868512649580e
Umaru2023 <147261554+Umaru2023@users.noreply.github.com>
2023-10-21 08:18:50 -0600

    Update libro.cs

--- libro.cs
+++ libro.cs
@@ -1,19 +1,34 @@
 using System;
 
-public class Libro
+class Libro
 {
-    // Propiedades
-    public string Título { get; set; }
+    public string Titulo { get; set; }
     public string Autor { get; set; }
 
-  
-   
+    public Libro(string titulo, string autor)
+    {
+        Titulo = titulo;
+        Autor = autor;
+    }
+
+    public void MostrarInformacion()
+    {
+        Console.WriteLine("Título: " + Titulo);
+        Console.WriteLine("Autor: " + Autor);
+    }
 }
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("¡Hola, mundo!");
+        Libro libro1 = new Libro("El Principito", "Antoine de Saint-Exupéry");
+        Libro libro2 = new Libro("Cien años de soledad", "Gabriel García Márquez");
+
+        Console.WriteLine("Información del Libro 1:");
+        libro1.MostrarInformacion();
+
+        Console.WriteLine("\nInformación del Libro 2:");
+        libro2.MostrarInformacion();
     }
 }
