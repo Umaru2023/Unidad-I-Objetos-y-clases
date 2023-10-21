63a9f5e0df08864b9e07e7ff913e1e92546932b0
Umaru2023 <147261554+Umaru2023@users.noreply.github.com>
2023-10-21 08:15:58 -0600

    Update empleado.cs

--- empleado.cs
+++ empleado.cs
@@ -1,22 +1,49 @@
 using System;
 
-public class Empleado
+class Empleado
 {
-   
+    // Propiedades
     public string Nombre { get; set; }
     public double Salario { get; set; }
 
-    
-    public double CalcularSalarioAnual()
+    // Constructor
+    public Empleado(string nombre)
     {
-        
+        Nombre = nombre;
+        Salario = 0; // Inicializamos el salario en 0
+    }
+
+    // Método para agregar el salario manualmente
+    public void AgregarSalarioManualmente()
+    {
+        Console.Write($"Introduce el salario de {Nombre}: ");
+        Salario = Convert.ToDouble(Console.ReadLine());
+    }
+
+    // Método para calcular el salario anual manualmente
+    public double CalcularSalarioAnualManual()
+    {
+        return Salario * 12;
     }
 }
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("¡Hola, mundo!");
+        // Crear objetos Empleado
+        Empleado empleado1 = new Empleado("Juan");
+        Empleado empleado2 = new Empleado("María");
+
+        // Agregar salarios manualmente
+        empleado1.AgregarSalarioManualmente();
+        empleado2.AgregarSalarioManualmente();
+
+        // Calcular el salario anual manualmente
+        double salarioAnualManualEmpleado1 = empleado1.CalcularSalarioAnualManual();
+        double salarioAnualManualEmpleado2 = empleado2.CalcularSalarioAnualManual();
+
+        Console.WriteLine($"El salario anual de {empleado1.Nombre} es: {salarioAnualManualEmpleado1:C}");
+        Console.WriteLine($"El salario anual de {empleado2.Nombre} es: {salarioAnualManualEmpleado2:C}");
     }
 }
