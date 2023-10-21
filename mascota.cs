fd44978f580cceb7f38aace7703cc2b18b5b3dc7
Umaru2023 <147261554+Umaru2023@users.noreply.github.com>
2023-10-21 17:54:28 -0600

    Update mascota.cs

--- mascota.cs
+++ mascota.cs
@@ -1,14 +1,18 @@
-using System;
+class Mascota:
+    def __init__(self, nombre, tipo, edad):
+        self.nombre = nombre
+        self.tipo = tipo
+        self.edad = edad
+    
+    def informacion(self):
+        print(f"Mi nombre es {self.nombre}, soy un {self.tipo} y tengo {self.edad} años.")
 
-public class Mascota
-{
-     
-}
+# Crear objetos de la clase Mascota
+mascota1 = Mascota("Luna", "perro", 3)
+mascota2 = Mascota("Simba", "gato", 5)
+mascota3 = Mascota("Nemo", "pez", 1)
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("¡Hola, mundo!");
-    }
-}
+# Mostrar información de las mascotas
+mascota1.informacion()
+mascota2.informacion()
+mascota3.informacion()
